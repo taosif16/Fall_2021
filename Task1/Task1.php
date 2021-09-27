3679eff7834bd83dbec7713caf408b73d4f67afb
taosif16 <87612941+taosif16@users.noreply.github.com>
2021-09-27 12:15:49 +0600

    Add files via upload

---
+++ Task1/Task1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html>
+<head>
+<h1>Registration Form<h1>
+</head>
+<body>
+<form>
+First name: <input type="text" name="First name">
+<br>
+<br>
+Lastname: <input type="text" name="Last name">
+<br>
+<br>
+
+</form>
+</body>
+</html>
