2faaba54d7399b98b1ae57808c75b86835d2b009
taosif16 <87612941+taosif16@users.noreply.github.com>
2021-10-11 12:27:52 +0600

    Update Task1.php

--- Task1/Task1.php
+++ Task1/Task1.php
@@ -1,17 +1,70 @@
-<!DOCTYPE>
+<?php include("action.php"); ?>
+ 
+ 
+<!DOCTYPE html>
 <html>
 <head>
-<h1>Registration Form<h1>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Form</title>
+        <link rel="stylesheet" href="">
 </head>
 <body>
-<form>
-First name: <input type="text" name="First name">
-<br>
-<br>
-Lastname: <input type="text" name="Last name">
-<br>
-<br>
-
-</form>
+        <h3>Registration Form</h3><hr>
+ 
+        <form action="" method="POST">
+                First Name :
+                <input type="text" name="fname"> <?php echo $valMsg; ?><br>
+ 
+                Last Name :
+                <input type="text" name="lname"> <?php echo $valMsg2; ?> <br>
+ 
+                Age :
+                <input type="text" name="age"><?php echo $ageVal; ?> <br>
+           
+            Designation :
+            <input type="radio" id="jp" name="Designation" value="Junior Programmer">
+                  <label for="jp">Junior Programmer</label>
+            <input type="radio" id="sp" name="Designation" value="Senior Programmer">
+                  <label for="sp">Senior Programmer</label>
+            <input type="radio" id="pl" name="Designation" value="Project Leader">
+                  <label for="pl">Project Leader</label>
+                  <br>
+ 
+                  <?php echo $radioVal; ?>
+ 
+                  <br>
+ 
+        Preferred Language:
+            <input type="checkbox" id="java" name="java" value="JAVA">
+            JAVA
+            <input type="checkbox" id="php" name="php" value="PHP">
+            PHP
+            <input type="checkbox" id="c++" name="c++" value="C++">    
+            C++  <br>
+ 
+            <?php echo $checkboxVal; ?>
+ 
+        <?php echo $java;?>
+ 
+        <?php echo $php;?>
+ 
+        <?php echo $c;?>
+ 
+        <br>
+ 
+        Email :
+                <input type="text" name="email"> <?php echo $valMsg3; ?> <br>
+ 
+    Password :
+                <input type="text" name="password"> <?php echo $valMsg4; ?> <br>
+ 
+        <input type="submit" name="submit" value="Sumbit">
+        <input type="reset" name="reset" value="Reset">
+ 
+        </form>
+ 
+ 
+       
 </body>
 </html>
